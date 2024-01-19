338d0409a945c641530f715afbf25240af0b0949
Hilmi Baskoparan <h.baskoparan@gmail.com>
2024-01-19 15:46:55 +0300

    Header and Footer Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import ListEmployee from './components/ListEmployee';
+import FooterComponent from './components/Footer';
+import HeaderComponent from './components/Header';
 
 function App() {
   return (
-    <div className="container">
-      <ListEmployee/>
+    
+    <div>
+      <Router>
+        <HeaderComponent />
+          <div className='container'>
+            <Switch>
+              <Route path = "/" exact component = {ListEmployee}></Route>
+              <Route path = "/employees" component = {ListEmployee}></Route>
+              <ListEmployee />
+            </Switch>
+          </div>
+          <FooterComponent />
+      </Router>
     </div>
   );
 }
