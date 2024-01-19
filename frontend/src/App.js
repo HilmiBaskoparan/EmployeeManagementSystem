4e67e9bfafbb1acdba0c3f808347fcfac8942774
Hilmi Baskoparan <h.baskoparan@gmail.com>
2024-01-19 17:00:11 +0300

    View Employee Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import ListEmployee from './components/ListEmployee';
 import FooterComponent from './components/Footer';
 import HeaderComponent from './components/Header';
 import CreateEmployee from './components/CreateEmployee';
+import ViewEmployee from './components/ViewEmployee';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
               <Route path = "/" exact component = {ListEmployee}></Route>
               <Route path = "/employees" component = {ListEmployee}></Route>
               <Route path = "/add-employee/:id" component = {CreateEmployee}></Route>
+              <Route path = "/view-employee/:id" component = {ViewEmployee}></Route>
               <ListEmployee />
             </Switch>
           </div>
